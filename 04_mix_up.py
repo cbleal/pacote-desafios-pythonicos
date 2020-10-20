82d447ab09ba5dac714430802ee71dd146719bf2
CLAUDINEI BORGES LEAL <38544732+cbleal@users.noreply.github.com>
2020-10-19 23:23:14 -0300

    Update 04_mix_up.py

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -14,7 +14,10 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(a) and len(b) >= 2:
+        a1 = a[:2]
+        b1 = b[:2]
+        return ' '.join([b1 + a[2:], a1 + b[2:]])      
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
